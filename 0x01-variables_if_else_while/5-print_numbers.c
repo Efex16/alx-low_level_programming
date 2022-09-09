001632272e0245b6769485e2fd55f7f37a2fd4da
Efex16 <efeefosa84@gmail.com>
2022-09-09 11:01:09 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int (num);
+	for (num = 0; num < 10; num++)
+	printf("%d", num);
+}
+putchar('\n');
+
+return (0);
+}
