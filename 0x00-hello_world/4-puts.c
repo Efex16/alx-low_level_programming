a3343bfd27235f79c74cd1bf120185ac5403b3e2
Efex16 <efeefosa84@gmail.com>
2022-09-08 17:07:01 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+#include <studio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: Displays a message to the terminal
+ * */
+    int main(void)
+{
+	puts("\" programming is like building a multilingual puzzle");
+	return (0);
+}
