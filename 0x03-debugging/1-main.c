2f9bd337a82e6319bb9c4e6bfcb83bdb788e0983
Efex16 <efeefosa84@gmail.com>
2022-09-16 02:50:23 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,17 +10,14 @@ int main(void)
 	int i;
 
 	printf("infinte loop incoming :(\n");
-
+	i = 0;
 	/**
-	 * i = 0;
-	 *
 	 * while (i < 0)
 	 * {
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("infinite loop avoided! \\o/\n");
 
-	return 0;
+	return (0);
 }
