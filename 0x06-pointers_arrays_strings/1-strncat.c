29c806fb19b3353343dd052fa8683c1e5234618c
Efex16 <efeefosa84@gmail.com>
2022-09-21 15:52:14 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: n
+ * Return: two concat strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i, co;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+
+	for (co = 0; co < n; co++)
+	{
+		dest[i + co] = src[co];
+		if (src[co] == '/0')
+			co = n;
+	}
+
+	return (dest);
+}
