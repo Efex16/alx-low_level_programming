99963cb68c93f86ff039df77303f92405658e2aa
Efex16 <111120611+Efex16@users.noreply.github.com>
2023-04-12 13:23:41 +0100

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Function that searches for a value in an
+ * array of integers using the Linear search algorithm
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
